c0cc59ddb0a0b5efea7cd37bf580198cb3c7e608
patrick thornton <patrickthornton@college.harvard.edu>
2024-04-28 18:53:47 -0400

    new post pretty

--- front/src/lib/slider.svelte
+++ front/src/lib/slider.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
     import { colorFromHue } from "$lib/hue.svelte";
 
-    export let starting_hue: number;
+    export let hue: number = 0;
 </script>
 
-<div
-    class="slider"
-    style="background-color: {colorFromHue(360 - starting_hue)};"
->
-    <input type="range" min="0" max="360" bind:value={starting_hue} />
+<div class="slider" style="background-color: {colorFromHue(hue)};">
+    <input type="range" min="0" max="360" bind:value={hue} />
 
-    current hue: {starting_hue}
+    current hue: {hue}
 </div>
 
 <style>
@@ -19,9 +16,6 @@
         flex-direction: column;
         justify-content: center;
         text-align: center;
-        position: fixed;
-        right: 13%;
-        top: 50%;
         color: white;
         border-radius: 0.5rem;
         padding: 1rem;
--- front/src/routes/(logged-in)/new/+page.svelte
+++ front/src/routes/(logged-in)/new/+page.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
     import Post from "$lib/interfaces.svelte";
+    import Slider from "$lib/slider.svelte";
     import { colorFromHue } from "$lib/hue.svelte";
     import { user_name } from "../../../stores/user.js";
     import { goto } from "$app/navigation";
 
-    let posttxt = "";
-    let posthue: number;
+    let post_text = "";
+    let post_hue = 0;
 
     let newpost: Post;
-    async function newPost() {
+    async function post() {
         let author: string = $user_name;
-        let text: string = posttxt;
-        let hue: number = posthue;
+        let text: string = post_text;
+        let hue: number = post_hue;
+
+        if (!text) {
+            return;
+        }
 
         await fetch("http://127.0.0.1:8000/post/post", {
             method: "POST",
@@ -22,29 +27,78 @@
         }).catch((err) => console.error(err));
 
         // reset the input after adding a post
-        posttxt = "";
+        post_text = "";
         goto("/");
     }
 </script>
 
-<div class="slider" style="background-color: {colorFromHue(360 - posthue)};">
-    <input type="range" min="0" max="360" bind:value={posthue} />
-
-    current hue: {posthue}
+<div class="backdrop" style="background-color: {colorFromHue(post_hue)}">
+    <form class="workbench" on:submit|preventDefault={post}>
+        <h2>new post</h2>
+        <Slider bind:hue={post_hue} />
+        <input
+            type="textarea"
+            class="post"
+            bind:value={post_text}
+            placeholder="i was just thinking..."
+        />
+        <button type="submit" class="submit">post!</button>
+    </form>
 </div>
 
-<h1>new post</h1>
-<input bind:value={posttxt} placeholder="new post" />
-<button on:click={newPost}>Add Post</button>
-
 <style>
-    .slider {
+    .backdrop {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .workbench {
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        justify-content: space-between;
+        align-items: center;
         text-align: center;
         color: white;
-        border-radius: 0.5rem;
+        border-radius: 1rem;
         padding: 1rem;
+        border: 1px solid white;
+        width: 20%;
+        height: 40%;
+        text-decoration: none;
+    }
+
+    .post {
+        width: 80%;
+        height: 100%;
+        margin: 1rem;
+        border-radius: 1rem;
+        border: 1px solid white;
+        padding: 1rem;
+        background-color: transparent;
+        color: white;
+        font-family: "Spectral", serif;
+        font-size: 12pt;
+        text-wrap: unrestricted;
+        overflow: hidden;
+        resize: vertical;
+    }
+
+    .submit {
+        width: 20%;
+        height: 10%;
+        margin: 1rem;
+        border-radius: 0.5rem;
+        border: 1px solid white;
+        background-color: rgb(255, 255, 255, 0.15);
+        color: white;
+        font-family: "Spectral", serif;
+        font-size: 12pt;
+    }
+
+    .submit:hover {
+        background-color: rgb(255, 255, 255, 0.3);
     }
 </style>
