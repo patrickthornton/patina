0a6de538a52920eb371b3a67d77c93746cb935db
patrick thornton <patrickthornton@college.harvard.edu>
2024-04-29 01:31:09 -0400

    extra button finesse

--- front/src/lib/button_gradient.svelte
+++ front/src/lib/button_gradient.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
     import { page } from "$app/stores";
+    import { user_id } from "../stores/user.js";
 
     $: onFollowing = $page.url.pathname === "/following";
 </script>
 
-{#if onFollowing}
-    <a class="button enabled" href="/"> gradient </a>
+{#if $user_id != 0}
+    {#if onFollowing}
+        <a class="button enabled" href="/"> gradient </a>
+    {:else}
+        <a class="button enabled" href="/following"> following </a>
+    {/if}
 {:else}
-    <a class="button enabled" href="/following"> following </a>
+    <div class="button disabled">log in!</div>
 {/if}
 
 <style>
